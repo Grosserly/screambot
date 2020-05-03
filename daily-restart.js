3ba56146d67dff7498195ed2e56495aca3a42cd1
garlicOS® <14845347+the-garlic-os@users.noreply.github.com>
2020-05-03 18:42:20 -0500

    Add files via upload

---
+++ daily-restart.js
@@ -0,0 +1,15 @@
+const { CronJob } = require("cron");
+const job = new CronJob({
+	cronTime: "00 00 00 * * *",
+	onTick: () => {
+		/*
+		* Restart Screambot every midnight.
+		*/
+		console.info("*** Another day, another fresh instance of Screambot! Restarting... ***");
+		process.exit();
+	},
+	start: false,
+	timeZone: "Etc/UTC"
+});
+
+job.start();
