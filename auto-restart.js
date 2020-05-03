dc01c8f0e6c352feaca86b7cf3c83e6e0c78f493
garlicOS® <14845347+the-garlic-os@users.noreply.github.com>
2020-05-03 18:46:03 -0500

    Update auto-restart.js

--- auto-restart.js
+++ auto-restart.js
@@ -3,8 +3,8 @@ const job = new CronJob({
 	cronTime: "00 00 00 * * *",
 	onTick: () => {
 		/*
-		* Restart Screambot every midnight.
-		*/
+		 * Restart Screambot every midnight.
+		 */
 		console.info("*** Another day, another fresh instance of Screambot! Restarting... ***");
 		process.exit();
 	},
